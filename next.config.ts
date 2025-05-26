252ab006f87b90245b8f652d3f88f54c91873a6f
PerceptLabs <shoshi4277@gmail.com>
2025-05-25 20:08:11 -0500

    Add next.config.ts 1748221692291

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+    ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+}
+
+export default nextConfig
